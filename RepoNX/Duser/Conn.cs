3801c44d428f79e9f234b261ab5c5a938c9f90d1
useyoursalt <useyoursalt@gmail.com>
2015-12-11 10:14:27 +0100

    Hejsa

---
+++ RepoNX/Duser/Conn.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace RepoNX
+{
+    public static class Conn
+    {
+        
+        public static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("server=194.255.108.50;database=dbnikon_web115_11;uid=nikon_web115_11;pwd=eGkYhBx3;MultipleActiveResultSets=True");
+            return con;
+        }
+
+        public static SqlConnection CreateConnection()
+        {
+            var cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
