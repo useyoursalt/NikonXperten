580cbb867c56a7a284a5bd4eab6b8c42b77157ec
useyoursalt <useyoursalt@gmail.com>
2015-12-14 10:56:23 +0100

    j

---
+++ NikonXperten/Areas/Admin/Controllers/AdminDefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NikonXperten.Areas.Admin.Controllers
+{
+    public class AdminDefaultController : Controller
+    {
+        // GET: Admin/AdminDefault
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
