c1cd83b061986d1778cd2f3b37f940000675ab19
useyoursalt <useyoursalt@gmail.com>
2015-12-15 10:34:01 +0100

    d

--- NikonXperten/Areas/Admin/Controllers/aKategoriController.cs
+++ NikonXperten/Areas/Admin/Controllers/aKategoriController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RepoNX;
 
 namespace NikonXperten.Areas.Admin.Controllers
 {
@@ -12,6 +13,50 @@ namespace NikonXperten.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddKat(Kategori kat)
+        {
+            if (ModelState.IsValid)
+            {
+                kf.Insert(kat);
+                ViewBag.MSG = "Din Kategori er blevet oprettet";
+            }
+            else
+            {
+                ViewBag.MSG = "Du skal udfylde alle felter";
+            }
+
+
+            return View("Index");
+        }
+
+        public ActionResult Edit()
+        {
+            return View(kf.GetAll());
+        }
+
+        public ActionResult EditForm(int id)
+        {
+            return View(kf.Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditResult(Kategori kat)
+        {
+            if (ModelState.IsValid)
+            {
+                kf.Update(kat);
+            }
+
+            return RedirectToAction("Edit");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            kf.Delete(id);
+            return RedirectToAction("Edit");
         }
     }
 }
--- RepoNX/Models/BaseModels/Kategori.cs
+++ RepoNX/Models/BaseModels/Kategori.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RepoNX.Models.BaseModels
+namespace RepoNX
 {
    public class Kategori
     {
