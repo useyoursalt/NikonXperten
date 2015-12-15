a010f911acc3064d635b4602466676294502355a
useyoursalt <useyoursalt@gmail.com>
2015-12-15 09:44:29 +0100

    j

---
+++ NikonXperten/Areas/Admin/Controllers/aKategoriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NikonXperten.Areas.Admin.Controllers
+{
+    public class aKategoriController : Controller
+    {
+        // GET: Admin/aKategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
