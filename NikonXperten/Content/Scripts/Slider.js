92126cba95430fabbdb657e3001b08a9c177ca38
Rasmus Rubin Nemborg <rasmus@nemnyk.dk>
2015-12-16 10:40:40 +0100

    cfd

---
+++ NikonXperten/Content/Scripts/Slider.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    var amount = $("img", ".SliderWrap").length - 1;
+    var width = 960;
+    var moved = -960;
+    var i = 0;
+
+    window.setInterval(function () {
+
+        if (i == amount) {
+            $("#pusher").css({
+                "margin-left": "5px"
+            })
+
+            moved = 0
+            i = -1;
+        }
+
+        else {
+
+            $("#pusher").animate({
+                "margin-left": moved + "px"
+            }, 2000)
+
+            i += 1;
+            moved -= 960;
+
+        }
+
+    }, 4000)
+
+})
